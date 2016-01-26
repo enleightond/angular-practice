05f6ee75078e490b22a7ca82c656242bb2dd511c
enleightond <enleightond@yahoo.com>
2016-01-26 15:44:07 -0800

    added user input for reddit clone, stores previous posts

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 var application = angular.module('reddit', ['ngAnimate']);
 
-app.controller('RedditController', function($scope) {
-	$scope.view.sortableFields = ['votes', 'date', 'title'];
-	$scope.view.posts[
-	{
-		title:"First title",
-		author:"An author",
-		image:"https://placebear.com",
-		description:"a lot of test parameters"
-		date:moment().subtract(2, 'days').subtract(3, 'hours').calendar(),
-	}]
-}
+application.controller('redditController', function($scope) {
+	$scope.newPost = {};
+	$scope.view = {};
+	$scope.view.sortOptions = ['votes', 'date', 'title'];
+	$scope.view.currentSort = $scope.view.sortOptions[0];
+	$scope.view.searchText = "";
+	$scope.view.newPostShow = false;
+	$scope.posts = [];
+	$scope.addVote = 0;
+
+	$scope.checkForError = function(input){
+		if (input.$valid || input.$untouched) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	$scope.newPostShow = function() {
+		$scope.view.newPostShow = true;		
+	}
+
+	$scope.addPost = function (form){
+		if (form.$valid) {
+			$scope.posts.push($scope.newPost);
+			$scope.newPost = {};
+			console.log($scope.posts)
+			$scope.view.newPostShow = false;
+		}
+	}
+
+})
