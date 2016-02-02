5e01046d3dcdf18d07ed02ff3019eb84efe5efbd
enleightond <enleightond@yahoo.com>
2016-02-02 11:36:09 -0800

    individual movies now correctly populate when selected from movie list

--- unit2/js/controllers.js
+++ unit2/js/controllers.js
@@ -13,12 +13,10 @@ app.controller('OmdbController', function($scope, $http, $location, MovieList){
 }); 
 
 app.controller("ResultController", function($scope, $http, $location, MovieList){
-	$scope.movieClick = function(data){
-		$scope.movieList.results.data.Title
-		console.log($scope.movieList.results.data.Title);
-		$scope.movieList.results.data.Poster;
-		console.log($scope.movieList.results.data.Poster);
-		$location.path('/movie');
+
+
+	$scope.movieClick = function(movieId){
+		$location.path(movieId+'/movie');
 	}
 	$scope.movieList = MovieList;
 });
@@ -27,20 +25,15 @@ app.controller("MovieController", function($scope, $http, $location,  MovieList)
 	var movieHax = $location.$$path.split('/')[1];
 	$http.get('http://www.omdbapi.com/?i=' + movieHax).then(function(data){
 
-		$scope.titleData = data.data.Title;
-		$scope.posterData = data.data.Poster;
+		// $scope.titleData = data.data.Title;
+		// $scope.posterData = data.data.Poster;
+		$scope.allData = data.data
 	})
 
 });
 
 app.factory('MovieList', function(){
 	var searchlist = {};
-	searchlist.results = [];
-	return searchlist;
-})
-
-app.factory('Movie', function(){
-	var movie = {};
 	searchlist.results = [];
 	return searchlist;
 })
