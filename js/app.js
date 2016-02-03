73e35321a314b68998678d3d3e79e8649bed7486
Jess Yang <jessyang650@users.noreply.github.com>
2016-02-02 16:11:33 -0800

    created shopping cart template

--- js/app.js
+++ js/app.js
@@ -3,12 +3,10 @@ var app = angular.module('teaStore', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: "partials/index.html",
+		templateUrl: "partials/order.html",
 		controller: "Selection"
 	}).when('/checkout', {
 		templateUrl: "partials/checkout.html",
 		controller: "Checkout"
-	}).otherwise({
-		redirectTo: "/"
 	});
 })
